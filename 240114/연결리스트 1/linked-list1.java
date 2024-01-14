45a11e6a907892e25e7cc23d68cd20f225a6c89f
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2024-01-14 12:58:22 +0900

    Title: 연결리스트 1, Time: 3107ms, Memory: 106MB, Status: Passed - CodeTree

--- 240114/연결리스트 1/linked-list1.java
+++ 240114/연결리스트 1/linked-list1.java
@@ -1,62 +1,104 @@
-import java.util.* ; 
-
-class Node{
-    String data ; 
-    Node prev, next ; 
-
-    Node(String data){
-        this.data = data ; 
-        this.prev = this.next = null ; 
-    }
-}
+import java.util.Scanner;
 
 public class Main {
-    public static int n ; 
+    // 한 노드를 나타내는 클래스입니다.
+    static class Node {
+        // 문자열을 값으로 가집니다.
+        String data;
+        Node prev, next;
+
+        // 생성자를 사용하여 Node를 초기화합니다.
+        public Node(String data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    // 두 노드를 연결해줍니다.
+    public static void connect(Node s, Node e) {
+        if (s != null) s.next = e;
+        if (e != null) e.prev = s;
+    }
+
+    // target 뒤에 s를 삽입합니다.
+    public static void insertNext(Node target, Node s) {
+        connect(s, target.next);
+        connect(target, s);
+    }
+
+    // target 앞에 s를 삽입합니다.
+    public static void insertPrev(Node target, Node s) {
+        connect(target.prev, s);
+        connect(s, target);
+    }
+
+    // target의 이전 노드, target, target의 다음 노드의 값을 출력합니다.
+    public static void printNode(Node target) {
+        String n = "(Null)";
+
+        // 이전 노드가 존재하지 않는다면, Null을 출력합니다.
+        // 아니라면, 이전 노드의 값을 출력합니다.
+        if (target.prev == null) System.out.print(n + " ");
+        else System.out.print(target.prev.data + " ");
+
+        // target의 값을 출력합니다.
+        System.out.print(target.data + " ");
+
+        // 다음 노드가 존재하지 않는다면, Null을 출력합니다.
+        if (target.next == null) System.out.println(n);
+        else System.out.println(target.next.data);
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ; 
-        String S_init = sc.next() ; 
-        Node cur = new Node(S_init) ; 
+        Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt() ; 
-        for(int i = 0 ; i < n ; i++){
-            String command = sc.next() ; 
+        // 맨 처음 문자열을 입력 받습니다.
+        String sInit = sc.nextLine();
 
-            if(command.contains("1")){
-                String S_value = sc.next() ; 
-                Node singleTon = new Node(S_value) ; 
-                
-                singleTon.next = cur ; 
-                singleTon.prev = cur.prev ; 
-                cur.prev = singleTon ; 
+        // 맨 처음 존재하는 노드를 만듭니다.
+        Node cur = new Node(sInit);
 
+        // 연산의 개수를 입력 받습니다.
+        int n = sc.nextInt();
 
-            }else if(command.contains("2")){
-                String S_value = sc.next() ; 
-                Node singleTon = new Node(S_value) ; 
+        while (n-- > 0) {
+            int option = sc.nextInt();
 
-                singleTon.prev = cur ; 
-                singleTon.next = cur.next ; 
-                cur.next = singleTon ; 
+            // option이 1이라면, cur의 앞에 노드를 삽입합니다.
+            if (option == 1) {
+                // 삽입할 노드를 만듭니다.
+                String data = sc.next();
+                Node target = new Node(data);
 
-            }else if(command.contains("3")){ // 앞 노드랑 바꾸기
-                if(!(cur.prev == null)){
-                    cur.next = cur.prev ; 
-                    cur.prev = cur.prev.prev ; 
-                    cur.next.prev = cur ; 
-                    cur.next.next = cur.next ;     
-                }
-            }else if(command.contains("4")){ // 뒤 노드랑 바꾸기 
-                if(!(cur.next == null)){
-                    cur.prev = cur.next ;  // B : cur.next  --> cur.prev 
-                    cur.next = cur.next.next; // A ' next = 기존 B'next 
-                    cur.prev.next = cur ;  // B' next = A 
-                    cur.prev.prev = cur.prev ;  // B'pre = A'prev 
-                }
+                // cur의 앞에 삽입합니다.
+                insertPrev(cur, target);
             }
-            String prev_data = (cur.prev == null)? "(Null)" : cur.prev.data ; 
-            String next_data = (cur.next == null)? "(Null)" : cur.next.data ; 
-            System.out.println(prev_data +" "+  cur.data + " "+ next_data) ;   
-        } 
+
+            // option이 2라면, cur의 뒤에 노드를 삽입합니다.
+            if (option == 2) {
+                // 삽입할 노드를 만듭니다.
+                String data = sc.next();
+                Node target = new Node(data);
+
+                // cur의 뒤에 삽입합니다.
+                insertNext(cur, target);
+            }
+
+            if (option == 3) {
+                // cur의 이전 노드가 존재한다면, cur을 cur의 이전 노드로 변경합니다.
+                if (cur.prev != null) cur = cur.prev;
+            }
+
+            if (option == 4) {
+                // cur의 다음 노드가 존재한다면, cur을 cur의 다음 노드로 변경합니다.
+                if (cur.next != null) cur = cur.next;
+            }
+
+            // 매 연산이 진행될 때마다 cur의 값을 출력합니다.
+            printNode(cur);
+        }
+
+        sc.close();
     }
 }
