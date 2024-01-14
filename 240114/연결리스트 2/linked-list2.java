f9eb4233b163d7431738000e46808c53e83ad456
Heemin0822 <132312673+Vida0822@users.noreply.github.com>
2024-01-14 13:27:56 +0900

    Title: 연결리스트 2, Time: 2437ms, Memory: 155MB, Status: Passed - CodeTree

--- 240114/연결리스트 2/linked-list2.java
+++ 240114/연결리스트 2/linked-list2.java
@@ -1,80 +1,88 @@
-import java.util.* ; 
+import java.util.Scanner;
 
 public class Main {
-static class Node{
-    int id ; 
-    Node prev, next ; 
+    // 최대 노드 개수
+    public static final int MAX_N = 100005;
 
-    public Node(int id) {
-        this.id = id ; 
-        this.prev= null ;
-        this.next = null ;  
+    // 한 노드를 나타내는 클래스입니다.
+    static class Node {
+        int id;
+        Node prev, next;
+
+        public Node(int id) {
+            this.id = id;
+            this.prev = null;
+            this.next = null;
+        }
     }
-}
 
+    public static Node[] nodes = new Node[MAX_N];
 
-// u 앞에 singleton을 삽입 
-public static void insertPrev(Node u, Node singleton ){
-    singleton.prev = u.prev ; 
-    singleton.next = u ; 
+    // u 앞에 singleton을 삽입합니다.
+    public static void insertPrev(Node u, Node singleton) {
+        singleton.prev = u.prev;
+        singleton.next = u;
 
-    if(null != singleton.prev)
-        singleton.prev.next = singleton ; 
-    if(null != singleton.next)
-        singleton.next.prev = singleton ; 
-}
+        if (null != singleton.prev)
+            singleton.prev.next = singleton;
+        if (null != singleton.next)
+            singleton.next.prev = singleton;
+    }
 
-public static void insertNext(Node u , Node singleton){
-    singleton.prev = u ; 
-    singleton.next = u.next ; 
+    // u 뒤에 singleton을 삽입합니다.
+    public static void insertNext(Node u, Node singleton) {
+        singleton.prev = u;
+        singleton.next = u.next;
 
-    if(null != singleton.prev)
-        singleton.prev.next = singleton ; 
-    if(null != singleton.next)
-        singleton.next.prev = singleton ; 
-}
+        if (null != singleton.prev)
+            singleton.prev.next = singleton;
+        if (null != singleton.next)
+            singleton.next.prev = singleton;
+    }
 
-public static void pop(Node u){
-    if(u.prev != null)
-        u.prev.next = u.next ; 
-    if(u.next != null)
-        u.next.prev = u.prev ; 
-}
+    // 노드 u를 제거합니다.
+    public static void pop(Node u) {
+        if (u.prev != null)
+            u.prev.next = u.next;
 
+        if (u.next != null)
+            u.next.prev = u.prev;
 
-    public static final int MAX_N = 100005 ; 
-    public static Node[] nodes = new Node[MAX_N] ; 
+        u.prev = u.next = null;
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ; 
-        int n = sc.nextInt() ; 
-        int q = sc.nextInt() ; 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
 
-        for(int i = 1 ; i<= n ; i++){
-            nodes[i] = new Node(i) ; 
+        // N 개의 단일 노드를 생성합니다.
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = new Node(i);
         }
 
-        for(int t = 0 ; t < q ; t++){
-            int type = sc.nextInt() ; 
-            int i = sc.nextInt() ;
+        // Q 개의 연산을 진행합니다.
+        for (int t = 0; t < q; t++) {
+            int type = sc.nextInt();
+            int i = sc.nextInt();
 
-            if( 1 == type){
-                pop(nodes[i]) ; 
-                // 원래같으면 그냥 찾으려면 prev, next 타고가면서 하나씩 맞는지 찾는 선형탐색 해야하는데, 
-                // 배열 인덱스에 맞춰 저장해놓으면 어떤 노드인지 바로 임의접근 가능  
-            }else if( 2 == type){
-                int j = sc.nextInt() ; 
-                insertPrev(nodes[i], nodes[j]) ; 
-            }else if(3 == type){
-                int j = sc.nextInt() ; 
-                insertNext(nodes[i] , nodes[j])  ; 
-            }else if(4==type){
-                System.out.print((nodes[i].prev == null ? 0: nodes[i].prev.id) +" ") ;
-                System.out.println(nodes[i].next == null ? 0: nodes[i].next.id)  ;   
+            if (1 == type) {
+                pop(nodes[i]);
+            } else if (2 == type) {
+                int j = sc.nextInt();
+                insertPrev(nodes[i], nodes[j]);
+            } else if (3 == type) {
+                int j = sc.nextInt();
+                insertNext(nodes[i], nodes[j]);
+            } else if (4 == type) {
+                System.out.print((nodes[i].prev == null ? 0 : nodes[i].prev.id) + " ");
+                System.out.println(nodes[i].next == null ? 0 : nodes[i].next.id);
             }
         }
 
-        for(int i = 1 ; i<= n ; i++)
-            System.out.print((nodes[i].next == null? 0:nodes[i].next.id) +" "); 
+        // 연산을 마친 후 1번 부터 N번 노드까지의 다음 노드 번호를 차례대로 한 줄에 출력합니다.
+        for (int i = 1; i <= n; i++)
+            System.out.print((nodes[i].next == null ? 0 : nodes[i].next.id) + " ");
+        sc.close();
     }
 }
